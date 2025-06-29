1270e740be5a8f183e856fff15c38f899673c4c0
k1ngk1te <kitewebdevelopers@gmail.com>
2025-06-29 16:04:06 +0100

    If a new token is not provided. Use the old one

--- src/server/utils/auth.ts
+++ src/server/utils/auth.ts
@@ -1,6 +1,5 @@
 import { CSRF_TOKEN } from '~/config/app';
 import type { LoginResponseType, ServerLoginResponseType } from '~/types';
-import AppError from '~/utils/errors/app-error';
 import HttpInstance from '~/utils/http';
 import { NewSuccessDataResponse } from './response';
 
@@ -14,12 +13,11 @@ export async function saveCredentials(
 
   const responseData = response.data;
 
-  // Get the CSRF_TOKEN FROM THE HEADERS
+  // Get the CSRF_TOKEN FROM THE HEADERS IF PROVIDED
   const newCsrfToken =
     typeof response.headers.get === 'function' ? response.headers.get(CSRF_TOKEN)?.toString() : undefined;
-  if (!newCsrfToken) throw new AppError(400, 'CSRF TOKEN was not provided');
 
-  const result: LoginResponseType['data'] = { ...credentials, csrfToken: newCsrfToken };
+  const result: LoginResponseType['data'] = { ...credentials, csrfToken: newCsrfToken || csrfToken };
 
   return NewSuccessDataResponse(result, responseData.message);
 }
