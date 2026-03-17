8d7d6cb4fe3c24724a6c3f665f6eefdb3718277c
k1ngk1te <kitewebdevelopers@gmail.com>
2026-03-17 23:00:13 +0100

    Getting the auth tken from the auth store

--- src/server/services/auth.service.ts
+++ src/server/services/auth.service.ts
@@ -1,5 +1,18 @@
+import { AppError } from '~/utils/errors';
+import authStore from '~/store/listeners/auth';
 import { AuthRepository } from '../repositories/auth';
+import type { LoginRequestDataType } from '~/types';
 
 export const getAuth = AuthRepository.getAuth.bind(AuthRepository);
-export const login = AuthRepository.login.bind(AuthRepository);
-export const logout = AuthRepository.logout.bind(AuthRepository);
+
+export function login(data: LoginRequestDataType) {
+  const { csrfToken } = authStore.get();
+  if (!csrfToken) throw new AppError(500, 'CSRF Token is required. Refresh the page and try again.');
+  return AuthRepository.login({ csrfToken, data });
+}
+
+export function logout() {
+  const { csrfToken, token } = authStore.get();
+  if (!csrfToken || !token) throw new AppError(401, 'Not authenticated');
+  return AuthRepository.logout({ csrfToken, token });
+}
--- src/store/queries/auth.ts
+++ src/store/queries/auth.ts
@@ -2,63 +2,41 @@ import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 import * as AuthService from '~/server/services/auth.service';
 import type { LoginRequestDataType, LoginResponseType, LogoutResponseType, MutationOptionsType } from '~/types';
-import { AppError } from '~/utils/errors';
 
-import { useAuthContext, useUserContext } from '../contexts';
 import tags from '../tags';
 
 // ****** Queries ******
 
-// get auth status
 export function useGetAuthQuery({ initialData }: { initialData?: LoginResponseType }) {
-  const query = useQuery<LoginResponseType>({
+  return useQuery<LoginResponseType>({
     queryKey: [tags.Auth],
-    async queryFn() {
-      return AuthService.getAuth();
-    },
+    queryFn: AuthService.getAuth,
     initialData,
     retry: false,
   });
-
-  return query;
 }
 
 // ****** Mutations ******
 
-// login
 export function useLoginMutation(options: MutationOptionsType<LoginResponseType['data']>) {
-  const { csrfToken } = useAuthContext();
-  const mutation = useMutation({
-    async mutationFn(data: LoginRequestDataType) {
-      if (!csrfToken) {
-        window.location.href = window.location.href as string;
-        throw new AppError(500, 'CSRF Token is required. Refresh the page and try again.');
-      }
-      return AuthService.login({ csrfToken, data });
+  return useMutation({
+    mutationFn(data: LoginRequestDataType) {
+      return AuthService.login(data);
     },
     onSuccess(response) {
       options.onSuccess(response);
     },
   });
-
-  return mutation;
 }
 
-// logout
 export function useLogoutMutation(options: MutationOptionsType<LogoutResponseType['data']>) {
   const queryClient = useQueryClient();
 
-  const { csrfToken, token } = useUserContext();
-
-  const mutation = useMutation({
-    async mutationFn() {
-      return AuthService.logout({ csrfToken, token });
-    },
+  return useMutation({
+    mutationFn: AuthService.logout,
     onSuccess(response) {
       queryClient.clear();
       options.onSuccess(response);
     },
   });
-
-  return mutation;
 }
