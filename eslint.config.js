e4d5baf303a364336241a2d9f8ebeea6d0cbefd7
k1ngk1te <kitewebdevelopers@gmail.com>
2026-09-05 12:30:10 +0100

    Added prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,8 @@
 import js from '@eslint/js';
-import globals from 'globals';
+import eslintConfigPrettier from 'eslint-config-prettier';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
@@ -26,4 +27,5 @@ export default tseslint.config(
       'react/display-name': 'off',
     },
   },
+  eslintConfigPrettier,
 );
